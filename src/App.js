8688fc4d29423589387b1e5c75b323db8e2928eb
paulpelumi <118385674+paulpelumi@users.noreply.github.com>
2023-02-26 20:38:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react'
-// import { Routes, Route } from "react-router-dom";
-import Home from "./container/Home";
 import Login from "./components/Login";
-import { Routes, Route, useNavigate } from 'react-router-dom';
 
 
 const App = () => {
